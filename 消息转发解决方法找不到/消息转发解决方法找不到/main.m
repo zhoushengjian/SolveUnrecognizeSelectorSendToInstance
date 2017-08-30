8ec7dfb7e5210fa045842446581aab83832117ad
zhoushnegjian <654614401@qq.com>
2017-08-30 10:43:14 +0800

    重新上传

---
+++ 消息转发解决方法找不到/消息转发解决方法找不到/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  消息转发解决方法找不到
+//
+//  Created by zhoushengjian on 2017/8/13.
+//  Copyright © 2017年 zhuanbei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
