ec8ce3b22f71fa14a9cb3077a794f45a393b49ee
zhoushnegjian <654614401@qq.com>
2017-08-30 10:08:47 +0800

    upload code

---
+++ 消息转发解决方法找不到/消息转发解决方法找不到/NSObject+SJ.h
@@ -0,0 +1,13 @@
+//
+//  NSObject+SJ.h
+//  消息转发解决方法找不到
+//
+//  Created by zhoushengjian on 2017/8/14.
+//  Copyright © 2017年 zhuanbei. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSObject (SJ)
+
+@end
